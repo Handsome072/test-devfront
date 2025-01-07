239828cc8c3a183ccc7e994e95627e75c29cbe8b
Andson Rajaona <andsonandriamitovy@gmail.com>
2025-01-07 07:02:58 +0300

    fix space

--- resources/js/components/Footer.vue
+++ resources/js/components/Footer.vue
@@ -13,21 +13,21 @@
 
     <div class="bg-gray-900 text-white py-4 border-t border-gray-800">
       <div class="flex items-center justify-center gap-4">
-        <p class="text-sm text-gray-300">
+        <p class="text-gray-300">
           Sur <strong>8027 avis clients</strong>, AAAEP.fr a obtenu la note de
           <span class="text-green-500 font-bold">4.4/5</span>
         </p>
         <img src="@/images/trustpilot2.png" alt="Trustpilot logo" class="h-6" />
-        <a href="#" class="text-blue-400 text-sm underline">
+        <a href="#" class="text-blue-400 underline">
           Voir tous les avis clients aaaep.fr
         </a>
       </div>
     </div>
 
     <div class="container mx-auto px-4 py-4">
-      <div class="grid grid-cols-1 md:grid-cols-4 gap-6 text-gray-700 text-sm">
+      <div class="grid grid-cols-1 md:grid-cols-4 gap-6  ">
         <div class="mt-5">
-          <h4 class="font-bold mb-2">Tous les tests psychotechniques</h4>
+           <h4 class="font-bold mb-2">Tous les tests psychotechniques</h4>
           <ul class="space-y-2">
             <li class="flex items-start">
               <span class="text-gray-500 mr-2">»</span>
@@ -262,5 +262,8 @@ export default {
 <style scoped>
 .sponsor img {
   width: 120px;
+}
+h4{
+  font-family: "Lato", sans-serif;
 }
 </style>
